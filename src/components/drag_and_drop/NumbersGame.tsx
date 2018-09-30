326f4db0684664aebf7e0fca9c3ce14b89e90050
Alejandro Sobko <alejandro.sobko@gmail.com>
2018-09-30 12:21:32 -0300

    Delete unnecessary logic

--- src/components/drag_and_drop/NumbersGame.tsx
+++ src/components/drag_and_drop/NumbersGame.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import ClapsSound from '../../assets/sounds/claps.wav';
 import MoveSound from '../../assets/sounds/move.wav';
-import initialData from '../../initial-data';
 import VerticalColumn from '../../styles/VerticalColumn';
 
 
@@ -11,22 +10,37 @@ export default class NumbersGame extends React.Component<any, any> {
     super(props);
     this.onDragEnd = this.onDragEnd.bind(this);
 
-    this.state = initialData;
+    this.state = {
+      column: {
+        id: 'column-1',
+        numberIds: ['four', 'one', 'five', 'three', 'two'],
+      },
+      numbers: {
+        'five': { id: 'five', content: '5' },
+        'four': { id: 'four', content: '4' },
+        'one': { id: 'one', content: '1' },
+        'three': { id: 'three', content: '3' },
+        'two': { id: 'two', content: '2' },
+      },
+    };
   }
 
   public onDragEnd(result: any) {
     const { destination, source, draggableId } = result;
     const samePosition = destination === null;
-
-    if (!destination || samePosition) { return; }
-
-    const column = this.state.columns[destination.droppableId];
+    const column = this.state.column;
     const numberIds = Array.from(column.numberIds);
     numberIds.splice(source.index, 1);
     numberIds.splice(destination.index, 0, draggableId);
-
     const numbers = numberIds.map((numberId: string) => parseInt(this.state.numbers[numberId].content, 10));
 
+    if (!destination || samePosition) { return; }
+
+    this.playSound(numbers);
+    this.updateState(column, numberIds);
+  }
+
+  public playSound(numbers: any) {
     if (isSortedAsc(numbers)) {
       const audio = new Audio(ClapsSound);
       audio.play();
@@ -34,39 +48,29 @@ export default class NumbersGame extends React.Component<any, any> {
       const audio = new Audio(MoveSound);
       audio.play();
     }
+  }
 
+  public render() {
+    const numbers = this.state.column.numberIds.map((numberId: number) => this.state.numbers[numberId]);
+    return (
+      <div key={this.state.column.id}>
+        <h2>Arrastrar</h2>
+        <h3>Ordená las cajas de mayor a menor</h3>
+        <VerticalColumn column={this.state.column} items={numbers} onDragEnd={this.onDragEnd} />
+      </div>
+    )
+  }
+
+  private updateState(column: any, numberIds:any) {
     const newColumn = {
       ...column,
       numberIds
     };
 
-    const newState = {
+    this.setState({
       ...this.state,
-      columns: {
-        ...this.state.columns,
-        [newColumn.id]: newColumn,
-      }
-    };
-
-    this.setState(newState);
-  }
-
-  public render() {
-    return (
-      this.state.columnOrder.map((columnId: string) => {
-        const column = this.state.columns[columnId];
-        const numbers = column.numberIds.map((numberId: number) => this.state.numbers[numberId]);
-
-        return (
-          <div key={column.id}>
-            <h2>Arrastrar</h2>
-            <h3>Ordená las cajas de mayor a menor</h3>
-            <VerticalColumn column={column} items={numbers} onDragEnd={this.onDragEnd} />
-          </div>
-        )
-      })
-
-    )
+      column: newColumn
+    });
   }
 
 }
--- src/initial-data.ts
+++ src/initial-data.ts
@@ -1,19 +1,3 @@
-const initialData = {
-  columnOrder: ['column-1'],
-  columns: {
-    'column-1': {
-      id: 'column-1',
-      numberIds: ['four', 'one', 'five', 'three', 'two'],
-      title: 'numbers',
-    }
-  },
-  numbers: {
-    'five': { id: 'five', content: '5' },
-    'four': { id: 'four', content: '4' },
-    'one': { id: 'one', content: '1' },
-    'three': { id: 'three', content: '3' },
-    'two': { id: 'two', content: '2' },
-  },
-};
+const initialData = {};
 
 export default initialData;
