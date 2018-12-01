be34c825599a6967cd19bb5351bfa3265a9f6218
Alejandro Sobko <alejandro.sobko@gmail.com>
2018-11-30 23:59:29 -0300

    Change welcome msg

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,5 @@ import * as React from 'react';
 export default (props: any) =>
   <div>
     <h1>¡Bienvenido a TikiJuegos!</h1>
-    <h3>Una página llena de juegos para los más pequeños. Juegos de arrastrar, pintar y encontrar objetos perdidos.</h3>
-    <h3>Es gratis, y siempre lo será :)</h3>
-    <h3>No hay puntajes ni competencia, sólo diversión y aprendizaje</h3>
+    <h3>Un sitio gratis de juegos infantiles sin puntajes ni competencia</h3>
   </div>
