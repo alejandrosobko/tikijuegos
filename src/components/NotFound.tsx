0f2bc481b547bc2984bf84130bc2d6f1fb4a9084
Alejandro Sobko <alejandro.sobko@gmail.com>
2020-06-14 16:33:02 -0300

    Enhance 404 page

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -3,6 +3,7 @@ import NotFoundImage from '../assets/images/imagen-404.jpeg'
 
 export default (props: any) =>
   <div className="container not-found">
+    <span>Error 404</span>
     <h1>Acá no hay juegos!</h1>
     <img src={NotFoundImage} alt="not found" />
   </div>
